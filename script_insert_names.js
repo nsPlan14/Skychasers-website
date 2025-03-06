13db9c74fb514e045eb0729d71ee3b0a3b002c9a
nsPlan14 <nizar.souit06@gmail.com>
2025-03-06 17:52:56 +0100

    Adding launch your name section
    New section fully integrated

--- script_insert_names.js
+++ script_insert_names.js
@@ -5,36 +5,36 @@ const supabaseUrl = "https://rxsbzajqlqtanegpwnze.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4c2J6YWpxbHF0YW5lZ3B3bnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwNjAwMDgsImV4cCI6MjA1NjYzNjAwOH0.9aF724obCLlOk5yIqIqrfh3pkPuXlxC6A-3YQd85ibU";
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
-// Funzione per salvare il nome, IP e data nel database
+// Function to save name, ip and time/date
 async function salvaNome() {
   const nomeInput = document.getElementById("nome").value;
   if (!nomeInput) {
-    alert("Devi inserire un nome!");
+    alert("You have to enter a name!");
     return;
   }
 
-  // Ottenere l'IP dell'utente
+  // IP
   try {
     const response = await fetch("https://api64.ipify.org?format=json");
     const { ip } = await response.json();
 
-    // Ottenere la data attuale in formato UTC
+    // Date (UTC)
     const nowUTC = new Date().toISOString();
 
-    // Inserire il nome, l'IP e la data nel database
+    // Name, IP and date into the database
     const { data, error } = await supabaseClient
       .from("nomi")
       .insert([{ nome: nomeInput, ip_address: ip, time_save: nowUTC }]);
 
     if (error) {
-      console.error("Errore nel salvataggio:", error);
-      alert("Errore: " + error.message);
+      console.error("Error in saving:", error);
+      alert("Error: " + error.message);
     } else {
-      alert("Nome salvato con successo!");
-      document.getElementById("nome").value = ""; // Resetta il campo
+      alert("Name successfully saved!");
+      document.getElementById("nome").value = ""; // Resetta tye field
     }
   } catch (err) {
-    console.error("Errore nel recupero dell'IP:", err);
-    alert("Impossibile ottenere l'IP.");
+    console.error("IP retrieval error:", err);
+    alert("Unable to get IP.");
   }
 }
